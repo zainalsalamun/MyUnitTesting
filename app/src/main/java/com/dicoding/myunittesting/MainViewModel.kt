0bb85871e037a915a3ca8fc05e9cb460ee05e23c
zainalsalamun <zainalrtf@gmail.com>
2021-01-13 14:12:30 +0700

    init commit

---
+++ app/src/main/java/com/dicoding/myunittesting/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.dicoding.myunittesting
+
+class MainViewModel (private val cuboidModel: CuboidModel){
+
+    fun getCircumference(): Double = cuboidModel.getCircumrefence()
+
+    fun getSurfaceArea(): Double = cuboidModel.getSurfaceArea()
+
+    fun getVolume():Double = cuboidModel.getVolume()
+
+    fun save (l: Double, w : Double, h :Double){
+        cuboidModel.save(l,w,h)
+    }
+}
